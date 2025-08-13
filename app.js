73028fc92a19adbfcb0419e7e319384506ce6d2f
ForGotteNIU <maestroiu60@gmail.com>
2025-08-13 22:05:37 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ const newWirth2 = width + 4;
 const division = newWirth / newWirth2
 const volume = 2 ** 3;/*возведение в степень*/
 console.log(volume);
+console.log(volume);
+
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street + ', ' + 5); /*Объединение строк, тут при смешивании строк и числа число считается строкой*/
